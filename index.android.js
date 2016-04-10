fc5024e71ad6484f46797909c61696ddc23eea39
Dominic Mülhaupt <dominicmh@gmail.com>
2016-04-10 11:15:05 +0200

    use React.createClass instead of class extends Component

--- index.android.js
+++ index.android.js
@@ -1,6 +1,5 @@
 import React, {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
   View,
@@ -13,12 +12,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-class Kort extends Component {
-  constructor() {
-    super();
-    this.mixins = [Mapbox.Mixin];
-  }
+const Kort = React.createClass({
+  mixins: [Mapbox.Mixin],
   getInitialState() {
     return {
       center: {
@@ -39,16 +34,16 @@ class Kort extends Component {
         },
       }],
     };
-  }
+  },
   onUserLocationChange(location) {
     console.log(location);
-  }
+  },
   onLongPress(location) {
     console.log(location);
-  }
+  },
   onOpenAnnotation(annotation) {
     console.log(annotation);
-  }
+  },
   render() {
     return (
       <View style={styles.container}>
@@ -58,11 +53,13 @@ class Kort extends Component {
         <Text onPress={() => this.setCenterCoordinateAnimated(mapRef, 47.223, 8.817, 10)}>
           Go to HSR at zoom level 10
         </Text>
-        <Text onPress={() => this.addAnnotations(mapRef, [{
-          coordinates: [47.2, 8.8],
-          type: 'point',
-          title: 'This is a new marker',
-          id: 'foo' }])}
+        <Text
+          onPress={() => this.addAnnotations(mapRef, [{
+            coordinates: [47.2, 8.8],
+            type: 'point',
+            title: 'This is a new marker',
+            id: 'foo',
+          }])}
         >
           Add new marker
         </Text>
@@ -75,13 +72,15 @@ class Kort extends Component {
         <Text onPress={() => this.setTilt(mapRef, 50)}>
           Set tilt to 50
         </Text>
-        <Text onPress={() => this.setVisibleCoordinateBoundsAnimated(
-            mapRef, 40.712, -74.227, 40.774, -74.125, 100, 100, 100, 100)}
+        <Text
+          onPress={() => this.setVisibleCoordinateBoundsAnimated(
+            mapRef, 40.712, -74.227, 40.774, -74.125, 100, 100, 100, 100
+          )}
         >
           Set visible bounds to 40.7, -74.2, 40.7, -74.1
         </Text>
         <Mapbox
-          annotations={this.getInitialState().annotations}
+          annotations={this.state.annotations}
           style={styles.container}
           direction={0}
           rotateEnabled
@@ -91,15 +90,15 @@ class Kort extends Component {
           ref={mapRef}
           accessToken={'pk.eyJ1IjoiZG9taW5pY21oIiwiYSI6ImNpbTIwbHFqbjAwbTN3MW02bWNxbjI4YmEifQ.ZkVpEGDJZXDSmG6fuO8ZZA'}
           styleURL={'https://raw.githubusercontent.com/osm2vectortiles/osm2vectortiles/gh-pages/styles/bright-v8.json'}
-          centerCoordinate={this.getInitialState().center}
-          zoomLevel={this.getInitialState().zoom}
+          centerCoordinate={this.state.center}
+          zoomLevel={this.state.zoom}
           logoIsHidden
           attributionButtonIsHidden
           onOpenAnnotation={this.onOpenAnnotation}
         />
       </View>
     );
-  }
-}
+  },
+});
 
 AppRegistry.registerComponent('Kort', () => Kort);
