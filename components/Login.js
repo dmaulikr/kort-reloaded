36113cd942062727b517d4f187624c4ad9dd455d
mmelchio <mmelchio@hotmail.ch>
2016-04-27 12:44:07 +0200

    fix some lint errors

--- components/Login.js
+++ components/Login.js
@@ -1,25 +1,25 @@
-import React, {View, Text, StyleSheet, TouchableHighlight} from 'react-native'
-import Button from 'react-native-button'
-import {Actions} from 'react-native-router-flux'
+import React, { View, Text, StyleSheet } from 'react-native';
+import Button from 'react-native-button';
+import { Actions } from 'react-native-router-flux';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+});
 
 class Login extends React.Component {
-    render(){
-        return (
-            <View style={styles.container}>
-                <Text>Login page</Text>
-                <Button onPress={Actions.tabbar}>Go to TabBar page</Button>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Login page </Text>
+        <Button onPress={Actions.tabbar}>Go to TabBar page </Button>
+      </View>
+    );
+  }
 }
-
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-    }
-});
 
 module.exports = Login;
--- index.android.js
+++ index.android.js
@@ -1,43 +1,35 @@
-import React, { AppRegistry, Navigator, StyleSheet, Text, View, } from 'react-native';
+import React, { AppRegistry, Navigator, StyleSheet, Text, View } from 'react-native';
 
 var Map = require('./components/Map');
 
-import { Scene, Router, TabBar, Modal, Schema, Actions, Reducer, } from 'react-native-router-flux';
-import Login from './components/Login'
-import NavigationDrawer from './components/NavigationDrawer'
-import TabView from './components/TabView'
+import { Scene, Router, TabBar, Modal, Schema, Actions, Reducer } from 'react-native-router-flux';
+import Login from './components/Login';
+import NavigationDrawer from './components/NavigationDrawer';
+import TabView from './components/TabView';
+import TabIcon from './components/TabIcon';
 
 
 const styles = StyleSheet.create({
-    container: {flex:1, backgroundColor:'transparent',justifyContent: 'center',
-        alignItems: 'center',}
-
+  container: { flex: 1, backgroundColor: 'transparent', justifyContent: 'center',
+      alignItems: 'center' },
 });
 
-class TabIcon extends React.Component {
-    render(){
-        return (
-            <Text style={{color: this.props.selected ? 'red' :'black'}}>{this.props.title}</Text>
-        );
-    }
-}
-
-const reducerCreate = params=>{
-    const defaultReducer = Reducer(params);
-    return (state, action)=>{
-        console.log("ACTION:", action);
-        return defaultReducer(state, action);
-    }
+const reducerCreate = params => {
+  const defaultReducer = Reducer(params);
+  return (state, action) => {
+    console.log('ACTION:', action);
+    return defaultReducer(state, action);
+  };
 };
 
 class App extends React.Component {
   render() {
-    return <Router createReducer={reducerCreate} sceneStyle={{backgroundColor:'#F7F7F7'}}>
-      <Scene key="modal" component={Modal} >
+    return <Router createReducer={reducerCreate} sceneStyle={{ backgroundColor: '#F7F7F7' }}>
+      <Scene key="modal" component={Modal}>
         <Scene key='root' hideNavBar={true}>
-          <Scene key="login" component={Login} title="Login" initial={true} style={{flex:1, backgroundColor:'transparent'}}/>
+          <Scene key="login" component={Login} title="Login" initial={true} style={ { flex: 1, backgroundColor: 'transparent' } } />
           <Scene key="tabbar" component={NavigationDrawer}>
-            <Scene key="main" tabs={true} >
+            <Scene key="main" tabs={true}>
               <Scene key='missionsTab' component={TabView} title='Missions' hideNavBar={true} icon={TabIcon} />
               <Scene key='profileTab' component={TabView} title='Profile' hideNavBar={true} icon={TabIcon} />
               <Scene key='highscore' component={TabView} title='Highscore' hideNavBar={true} icon={TabIcon} />
