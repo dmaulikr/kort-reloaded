e6c6f2f15c79706b1c03c7cb4052da601b4517de
mmelchio <mmelchio@hotmail.ch>
2016-05-10 20:58:11 +0200

    updated naming

--- js/utils/ApiUrlBuilder.js
+++ js/utils/ApiUrlBuilder.js
@@ -25,11 +25,14 @@ export default function createRequestUrl(apiUrl, queryParameters, parameters) {
   requestUrl = requestUrl + apiUrl;
 
   if (!requestUrl.endsWith('/')) requestUrl += '/';
-  requestUrl += getQueryParametersString(queryParameters);
 
-  if (parameters !== null && parameters.length !== 0) {
+  if (queryParameters !== null && queryParameters.length !== 0 && queryParameters[0] !== null) {
+    requestUrl += getQueryParametersString(queryParameters);
+  }
+
+  if (parameters !== null && parameters.length !== 0 && parameters[0] !== null) {
     requestUrl += getParametersString(parameters);
   }
-  
+
   return requestUrl;
 }
