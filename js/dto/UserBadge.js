f65e606d4782f2aa690a098c2296a83deddd8e8f
Dominic Mülhaupt <dominicmh@gmail.com>
2016-05-26 21:10:06 +0200

    add User DTOs

---
+++ js/dto/UserBadge.js
@@ -0,0 +1,12 @@
+export default class UserBadge {
+  constructor(id, name, title, description, color, sorting, won, createDate) {
+    this.id = id;
+    this.name = name;
+    this.title = title;
+    this.description = description;
+    this.color = color;
+    this.sorting = sorting;
+    this.won = won;
+    this.createDate = createDate;
+  }
+}
