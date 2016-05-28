8e80b1a9906ec7820003015215392bbe0f7f740a
Dominic Mülhaupt <dominicmh@gmail.com>
2016-05-28 22:32:19 +0200

    add Answer

--- js/constants/ActionTypes.js
+++ js/constants/ActionTypes.js
@@ -1,6 +1,8 @@
 import keyMirror from 'keymirror';
 
 module.exports = keyMirror({
+  ANSWERS_LOAD: null,
+  ANSWERS_LOAD_FOR_TYPE: null,
   MISSIONS_LOAD: null,
   TASKS_LOAD: null,
   USER_LOAD: null,
