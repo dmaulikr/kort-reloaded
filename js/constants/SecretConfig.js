42c9a0c5cbb9b36a599861c792b81a8266ed56e4
mmelchio <mmelchio@hotmail.ch>
2016-06-03 16:32:10 +0200

    fix identifier

--- js/constants/SecretConfig.js
+++ js/constants/SecretConfig.js
@@ -1,11 +1,11 @@
 export default {
   // Mapbox
-  ACCESS_TOKEN: 'pk.eyJ1IjoiZG9taW5pY21oIiwiYSI6ImNpbTIwbHFqbjAwbTN3MW02bWNxbjI4YmEifQ.ZkVpEGDJZXDSmG6fuO8ZZA',
+  MAPBOX_ACCESS_TOKEN: '',
 
   // Auth
-  TEST_USER_ID: '4156',
-  TEST_SECRET: '680ebf81e9b139e894769b42cd57e077e35859c5',
+  TEST_USER_ID: '',
+  TEST_SECRET: '',
   // Google
-  GOOGLE_IOS_CLIENT_ID: '653755350671-uoj2ueqncnvpjmmjvvqu0fthrlnlpr8i.apps.googleusercontent.com',
-  GOOGLE_WEB_CLIENT_ID: '653755350671.apps.googleusercontent.com',
+  GOOGLE_IOS_CLIENT_ID: '',
+  GOOGLE_WEB_CLIENT_ID: '',
 };
