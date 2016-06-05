766ebdcdbf7dad8a2725c5ff20026f6148a9dc47
mmelchio <mmelchio@hotmail.ch>
2016-06-05 11:43:54 +0200

    added styling

--- js/components/shared/Loading.ios.js
+++ js/components/shared/Loading.ios.js
@@ -3,6 +3,17 @@ import { ActivityIndicatorIOS,
   StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  overlayContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    marginBottom: 45,
+  },
   centering: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -15,12 +26,14 @@ const styles = StyleSheet.create({
 
 const Loading = function () {
   return (
-    <ActivityIndicatorIOS
-      animating
-      style={[styles.centering, styles.gray, { height: 80 }]}
-      color="white"
-      size="large"
-    />
+    <View style={styles.overlayContainer}>
+      <ActivityIndicatorIOS
+        animating
+        style={[styles.centering, styles.gray, { height: 80 }]}
+        color="white"
+        size="large"
+      />
+    </View>
   );
 };
 
