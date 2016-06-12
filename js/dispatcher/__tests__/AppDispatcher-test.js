00a63a239b3a69fda706673ef60c529db1289225
Dominic Mülhaupt <dominicmh@gmail.com>
2016-06-12 18:28:15 +0200

    fix lint errors

--- js/dispatcher/__tests__/AppDispatcher-test.js
+++ js/dispatcher/__tests__/AppDispatcher-test.js
@@ -1,56 +1,54 @@
+/* eslint-disable no-use-before-define */
+
 jest.autoMockOff();
 
-describe('AppDispatcher', function() {
-  var AppDispatcher;
+describe('AppDispatcher', () => {
+  let AppDispatcher;
 
-  beforeEach(function() {
+  beforeEach(() => {
     AppDispatcher = require('../AppDispatcher').default;
   });
 
-  it('sends actions to subscribers', function() {
-    var listener = jest.genMockFunction();
+  it('sends actions to subscribers', () => {
+    const listener = jest.genMockFunction();
     AppDispatcher.register(listener);
 
-    var payload = {};
+    const payload = {};
     AppDispatcher.dispatch(payload);
     expect(listener.mock.calls.length).toBe(1);
     expect(listener.mock.calls[0][0]).toBe(payload);
   });
 
-  it('waits with chained dependencies properly', function() {
-    var payload = {};
-
-    var listener1Done = false;
-    var listener1 = function(pl) {
+  it('waits with chained dependencies properly', () => {
+    const listener1 = () => {
       AppDispatcher.waitFor([index2, index4]);
-      
+
       expect(listener2Done).toBe(true);
       expect(listener3Done).toBe(true);
       expect(listener4Done).toBe(true);
-      listener1Done = true;
     };
-    var index1 = AppDispatcher.register(listener1);
+    AppDispatcher.register(listener1);
 
-    var listener2Done = false;
-    var listener2 = function(pl) {
+    let listener2Done = false;
+    const listener2 = () => {
       AppDispatcher.waitFor([index3]);
       expect(listener3Done).toBe(true);
       listener2Done = true;
     };
-    var index2 = AppDispatcher.register(listener2);
+    const index2 = AppDispatcher.register(listener2);
 
-    var listener3Done = false;
-    var listener3 = function(pl) {
+    let listener3Done = false;
+    const listener3 = () => {
       listener3Done = true;
     };
-    var index3 = AppDispatcher.register(listener3);
+    const index3 = AppDispatcher.register(listener3);
 
-    var listener4Done = false;
-    var listener4 = function(pl) {
+    let listener4Done = false;
+    const listener4 = () => {
       AppDispatcher.waitFor([index3]);
       expect(listener3Done).toBe(true);
       listener4Done = true;
     };
-    var index4 = AppDispatcher.register(listener4);
+    const index4 = AppDispatcher.register(listener4);
   });
 });
