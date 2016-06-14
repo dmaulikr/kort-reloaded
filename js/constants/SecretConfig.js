6a9a2edffb24baafe03821159810d8fd7176cf53
dominicmh <dominicmh@users.noreply.github.com>
2016-06-14 20:37:54 +0200

    Update SecretConfig.js

--- js/constants/SecretConfig.js
+++ js/constants/SecretConfig.js
@@ -3,8 +3,6 @@ export default {
   MAPBOX_ACCESS_TOKEN: '', // eslint-disable-line max-len
 
   // Auth
-  TEST_USER_ID: '',
-  TEST_SECRET: '',
   // Google
   GOOGLE_IOS_CLIENT_ID: '',
   GOOGLE_WEB_CLIENT_ID: '',
