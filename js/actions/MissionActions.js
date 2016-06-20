67ccea03b737e255b043080001733d027feecacc
Dominic Mülhaupt <dominicmh@gmail.com>
2016-06-20 17:06:38 +0200

    remove redundant methods

--- js/actions/MissionActions.js
+++ js/actions/MissionActions.js
@@ -3,15 +3,6 @@ import AppDispatcher from '../dispatcher/AppDispatcher';
 import MissionLoader from '../data/MissionLoader';
 
 export default class MissionActions {
-  static loadMissions(latitude, longitude) {
-    MissionLoader.getMissions(latitude, longitude, (missions) => {
-      AppDispatcher.dispatch({
-        actionType: ActionTypes.MISSIONS_LOAD,
-        data: missions,
-      });
-    });
-  }
-
   static solveMission(mission, message, isUnsolvable) {
     MissionLoader.postMission(
       mission,
