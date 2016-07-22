561c855cc053566d5e247e0aa0c56fd86f669ebd
Dominic Mülhaupt <dominicmh@gmail.com>
2016-07-22 15:45:30 +0200

    don't load badges when loading user

--- js/actions/UserActions.js
+++ js/actions/UserActions.js
@@ -5,49 +5,14 @@ import UserLoader from '../data/UserLoader';
 import authenticationStore from '../stores/AuthenticationStore';
 
 export default class UserActions {
-  static _onUserDataLoaded(userWithoutBadges, userBadges) {
-    const user = userWithoutBadges;
-    user.badges = userBadges;
-
-    AppDispatcher.dispatch({
-      actionType: ActionTypes.USER_LOAD,
-      data: user,
-    });
-  }
-
   static loadUser(userId, userSecret) {
-    let userWithoutBadges;
-    let userBadges;
-    let userLoaded = false;
-    let badgesLoaded = false;
-
     UserLoader.getUser(
       userSecret,
       (user) => {
-        if (badgesLoaded) {
-          UserActions._onUserDataLoaded(user, userBadges);
-        } else {
-          userWithoutBadges = user;
-        }
-        userLoaded = true;
-      },
-      (error) => {
         AppDispatcher.dispatch({
-          actionType: ActionTypes.USER_ERROR_LOAD,
-          data: error,
+          actionType: ActionTypes.USER_LOAD,
+          data: user,
         });
-      }
-    );
-
-    UserLoader.getUserBadges(
-      userId,
-      (badges) => {
-        if (userLoaded) {
-          UserActions._onUserDataLoaded(userWithoutBadges, badges);
-        } else {
-          userBadges = badges;
-        }
-        badgesLoaded = true;
       },
       (error) => {
         AppDispatcher.dispatch({
